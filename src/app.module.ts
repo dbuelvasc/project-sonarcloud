b324ec9de372374baad2d6557f6832ab4d865d89
Juan David Torres <jd.torres2@uniandes.edu.co>
2024-09-01 16:10:03 -0500

    Fix: app module error

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantModule } from './restaurant/restaurant.module';
+import { RestaurantEntity } from './restaurant/restaurant.entity/restaurant.entity';
 import { GastronomicCultureModule } from './gastronomic-culture/gastronomic-culture.module';
+import { GastronomicCultureEntity } from './gastronomic-culture/gastronomic-culture.entity/gastronomic-culture.entity';
 import { CharacteristicproductModule } from './characteristicproduct/characteristicproduct.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { CharacteristicproductEntity } from './characteristicproduct/characteristicproduct.entity/characteristicproduct.entity';
 import { RecipeModule } from './recipe/recipe.module';
 import { RecipeEntity } from './recipe/recipe.entity/recipe.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [RestaurantModule, GastronomicCultureModule, CharacteristicproductModule, RecipeModule,
@@ -18,7 +21,7 @@ import { RecipeEntity } from './recipe/recipe.entity/recipe.entity';
     username: 'postgres',
     password: 'postgres',
     database: 'postgres',
-    entities: [RestaurantModule, GastronomicCultureModule, CharacteristicproductModule, RecipeModule],
+    entities: [RestaurantEntity, GastronomicCultureEntity, CharacteristicproductEntity, RecipeEntity],
     dropSchema: true,
     synchronize: true,
     keepConnectionAlive: true
