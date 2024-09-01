be4285e8c0cb845e745a1b8c6af1a37b2143eccb
dbuelvasc <d.buelvasc@uniandes.edu.co>
2024-09-01 16:38:42 -0400

    create module and entity for characteristic product

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { GastronomicCultureModule } from './gastronomic-culture/gastronomic-culture.module';
+import { CharacteristicproductModule } from './characteristicproduct/characteristicproduct.module';
 
 @Module({
-  imports: [RestaurantModule, GastronomicCultureModule],
+  imports: [RestaurantModule, GastronomicCultureModule, CharacteristicproductModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/characteristicproduct/characteristicproduct.entity/characteristicproduct.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { GastronomicCultureEntity } from '../../gastronomic-culture/gastronomic-culture.entity/gastronomic-culture.entity';
+
+@Entity()
+export class CharacteristicproductEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  history: string;
+
+  @Column()
+  category: string;
+
+  @ManyToOne(
+    () => GastronomicCultureEntity,
+    (gastronomicCulture) => gastronomicCulture.characteristicproducts)
+  gastronomicCulture: GastronomicCultureEntity;
+}
