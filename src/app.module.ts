6228f0ec416e2663cdf8cdfa9652cd49cbf4659a
Juan David Torres <jd.torres2@uniandes.edu.co>
2024-09-01 16:05:38 -0500

    feat: add recipe entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { RestaurantModule } from './restaurant/restaurant.module';
 import { GastronomicCultureModule } from './gastronomic-culture/gastronomic-culture.module';
 import { CharacteristicproductModule } from './characteristicproduct/characteristicproduct.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RecipeModule } from './recipe/recipe.module';
+import { RecipeEntity } from './recipe/recipe.entity/recipe.entity';
 
 @Module({
-  imports: [RestaurantModule, GastronomicCultureModule, CharacteristicproductModule,
+  imports: [RestaurantModule, GastronomicCultureModule, CharacteristicproductModule, RecipeModule,
   TypeOrmModule.forRoot({
     type: 'postgres',
     host: 'localhost',
@@ -16,11 +18,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     username: 'postgres',
     password: 'postgres',
     database: 'postgres',
-    entities: [RestaurantModule, GastronomicCultureModule, CharacteristicproductModule],
+    entities: [RestaurantModule, GastronomicCultureModule, CharacteristicproductModule, RecipeModule],
     dropSchema: true,
     synchronize: true,
     keepConnectionAlive: true
-  }),
+  }),  
 ],
   controllers: [AppController],
   providers: [AppService],
