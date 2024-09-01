8c0525f4f291d61141584e22491b2af682b50d88
dbuelvasc <d.buelvasc@uniandes.edu.co>
2024-09-01 18:58:06 -0400

    create GRUD service for characteristicproduct

---
+++ src/characteristicproduct/characteristicproduct.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CharacteristicproductService } from './characteristicproduct.service';
+
+describe('CharacteristicproductService', () => {
+  let service: CharacteristicproductService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CharacteristicproductService],
+    }).compile();
+
+    service = module.get<CharacteristicproductService>(CharacteristicproductService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
