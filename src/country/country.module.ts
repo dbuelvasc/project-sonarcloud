5b305dbd5dc6366c774a55eff1688f8960656991
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-09-01 22:41:24 -0500

    fix(country-recipe): include entities in modules

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CountryService } from './country.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CountryEntity } from './country.entity/country.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CountryEntity])],
   providers: [CountryService],
 })
 export class CountryModule {}
--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RecipeEntity } from './recipe.entity/recipe.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RecipeEntity])],
   providers: [RecipeService],
 })
 export class RecipeModule {}
