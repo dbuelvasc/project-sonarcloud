1b03c9ec1f36809247f786d801e6fd5d67fad83a
Juan David Torres <jd.torres2@uniandes.edu.co>
2024-09-12 23:31:41 -0500

    Feat: add relation country restaurant

--- src/country-restaurant/country-restaurant.controller.ts
+++ src/country-restaurant/country-restaurant.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    Param,
+    Post,
+    Put,
+    UseInterceptors,
+    UseGuards,
+  } from '@nestjs/common';
+import { CountryRestaurantService } from './country-restaurant.service';
+import { RestaurantEntity } from 'src/restaurant/restaurant.entity';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
 @Controller('country-restaurant')
-export class CountryRestaurantController {}
+@UseInterceptors(BusinessErrorsInterceptor)
+export class CountryRestaurantController {
+    constructor(private readonly countryRestaurantService: CountryRestaurantService) {}
+
+    @Get(':countryId/restaurants/:restaurantId')
+    async findRestaurantByCountryIdRestaurantId(@Param('countryId') countryId: string, @Param('restaurantId') restaurantId: string): Promise<RestaurantEntity> {
+        return await this.countryRestaurantService.findRestaurantFromCountry(countryId, restaurantId);
+    }
+
+    @Get(':countryId/restaurants')
+    async findRestaurantsByCountryId(@Param('countryId') countryId: string): Promise<RestaurantEntity[]> {
+        return await this.countryRestaurantService.findRestaurantsFromCountry(countryId);
+    }
+
+    @Post(':countryId/restaurants/:restaurantId')
+    async addRestaurantToCountry(@Param('countryId') countryId: string, @Param('restaurantId') restaurantId: string): Promise<RestaurantEntity> {
+        return await this.countryRestaurantService.addRestaurantToCountry(countryId, restaurantId);
+    }    
+
+    @Delete(':countryId/restaurants/:restaurantId')
+    @HttpCode(204)
+    async deleteRestaurantFromCountry(@Param('countryId') countryId: string, @Param('restaurantId') restaurantId: string): Promise<void> {
+        await this.countryRestaurantService.deleteRestaurantFromCountry(countryId, restaurantId);
+    }
+}
