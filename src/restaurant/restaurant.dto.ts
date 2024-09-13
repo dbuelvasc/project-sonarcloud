fcb70b3f33ea76a8398beb07a0ca56def6865ff2
Juan David Torres <jd.torres2@uniandes.edu.co>
2024-09-12 22:06:03 -0500

    Feat: add DTO

--- src/restaurant/restaurant.dto.ts
+++ src/restaurant/restaurant.dto.ts
@@ -1,1 +1,20 @@
-export class RestaurantDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class RestaurantDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    city: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    michelinStars: number;
+
+    @IsDate()
+    @IsNotEmpty()
+    awardDate: Date;  
+    
+}
