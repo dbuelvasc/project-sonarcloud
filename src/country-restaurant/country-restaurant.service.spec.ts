319aeea7f0f6ae0af85cebac29f6c310ca567888
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-09-18 23:54:43 -0500

    fix(country-restaurant-tests): include typeorm config

--- src/country-restaurant/country-restaurant.service.spec.ts
+++ src/country-restaurant/country-restaurant.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
+
+import { TypeOrmTestingConfig } from "@/shared/testing-utils/typeorm-testing-config";
 import { CountryRestaurantService } from "./country-restaurant.service";
 
 describe("CountryRestaurantService", () => {
@@ -6,6 +8,7 @@ describe("CountryRestaurantService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [CountryRestaurantService],
     }).compile();
 
