acaac325829d8355c07d8b4ec2013f5369b59770
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-09-19 18:54:09 -0500

    refactor(user): stay dry

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,13 +1,8 @@
 export class User {
-  id: number;
-  username: string;
-  password: string;
-  roles: string;
-
-  constructor(id: number, username: string, password: string, roles: string) {
-    this.id = id;
-    this.username = username;
-    this.password = password;
-    this.roles = roles;
-  }
+  constructor(
+    public id: number,
+    public username: string,
+    public password: string,
+    public roles: string,
+  ) {}
 }
