a63f712322f49ca2fcde9fb9f550a3ac33a46235
Juan David Torres <jd.torres2@uniandes.edu.co>
2024-09-18 23:40:30 -0500

    Feat: add authentication to country controller to test

--- src/country/country.controller.ts
+++ src/country/country.controller.ts
@@ -15,6 +15,10 @@ import { CountryDto } from './country.dto';
 import { CountryEntity } from './country.entity';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 import { plainToInstance } from 'class-transformer';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RoleGuard } from '../auth/roles/role.guard';
+import { Roles } from '../auth/roles/roles.decorator';
+import userRoles from './../shared/security/user_roles';
 
 @Controller('country')
 @UseInterceptors(BusinessErrorsInterceptor)
@@ -22,38 +26,38 @@ export class CountryController {
     constructor(private readonly countryService: CountryService) {}
 
     @Get()
-    //@UseGuards(JwtAuthGuard, RoleGuard)
-    //@Roles(userRoles.ADMIN, userRoles.FULL_READER)
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(userRoles.ADMIN, userRoles.FULL_READER)
     async findAll() {
         return await this.countryService.findAll();
     }
 
     @Get(':countryId')
-    //@UseGuards(JwtAuthGuard, RoleGuard)
-    //@Roles(userRoles.ADMIN, userRoles.LIMITED_READER)
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(userRoles.ADMIN, userRoles.LIMITED_READER)
     async findOne(@Param('countryId') countryId: string) {
         return await this.countryService.findOne(countryId);
     }
 
     @Post()
-    //@UseGuards(JwtAuthGuard, RoleGuard)
-    //@Roles(userRoles.ADMIN, userRoles.WRITER)
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(userRoles.ADMIN, userRoles.WRITER)
     async create(@Body() CountryDto: CountryDto) {
         const country: CountryEntity = plainToInstance(CountryEntity, CountryDto);
         return await this.countryService.create(country);
     }
 
     @Put(':countryId')
-    //@UseGuards(JwtAuthGuard, RoleGuard)
-    //@Roles(userRoles.ADMIN, userRoles.WRITER)
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(userRoles.ADMIN, userRoles.WRITER)
     async update(@Param('countryId') countryId: string, @Body() countryDto: CountryDto) {
         const country: CountryEntity = plainToInstance(CountryEntity, countryDto);
         return await this.countryService.update(countryId, country);
     }
 
     @Delete(':countryId')
-    //@UseGuards(JwtAuthGuard, RoleGuard)
-    //@Roles(userRoles.ADMIN, userRoles.DELETE)
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(userRoles.ADMIN, userRoles.DELETE)
     @HttpCode(204)
     async delete(@Param('countryId') countryId: string) {
         return await this.countryService.delete(countryId);
