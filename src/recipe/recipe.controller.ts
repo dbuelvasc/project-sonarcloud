6fc9c50cd61f44bcc72cf990ac7c7931618c562f
jjmontenegro <jj.montenegro@uniandes.edu.co>
2024-09-22 19:09:43 -0500

    feat(recipe): include recipe controller

---
+++ src/recipe/recipe.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UseInterceptors,
+} from "@nestjs/common";
+
+import {
+  BusinessErrorsInterceptor,
+  UUIDValidationInterceptor,
+} from "@/shared/interceptors";
+import { RecipeDto } from "./recipe.dto";
+import { RecipeService } from "./recipe.service";
+
+@Controller("recipe")
+@UseInterceptors(
+  BusinessErrorsInterceptor,
+  new UUIDValidationInterceptor("recipeId"),
+)
+export class RecipeController {
+  constructor(private readonly recipeService: RecipeService) {}
+
+  @Get()
+  async findAll() {
+    return await this.recipeService.findAll();
+  }
+
+  @Get(":recipeId")
+  async findOne(@Param("recipeId") recipeId: string) {
+    return await this.recipeService.findOne(recipeId);
+  }
+
+  @Post()
+  async create(@Body() recipeDto: RecipeDto) {
+    return await this.recipeService.create(recipeDto);
+  }
+
+  @Put(":recipeId")
+  async update(
+    @Param("recipeId") recipeId: string,
+    @Body() recipeDto: RecipeDto,
+  ) {
+    return await this.recipeService.update(recipeId, recipeDto);
+  }
+
+  @Delete(":recipeId")
+  @HttpCode(204)
+  async delete(@Param("recipeId") recipeId: string) {
+    return await this.recipeService.delete(recipeId);
+  }
+}
--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
-import { RecipeService } from "./recipe.service";
-import { RecipeEntity } from "./recipe.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { RecipeController } from "./recipe.controller";
+import { RecipeEntity } from "./recipe.entity";
+import { RecipeService } from "./recipe.service";
 
 @Module({
   providers: [RecipeService],
   imports: [TypeOrmModule.forFeature([RecipeEntity])],
+  controllers: [RecipeController],
 })
 export class RecipeModule {}
