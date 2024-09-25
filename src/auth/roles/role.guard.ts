53c7b03bfa34afa113f211ea51093e50b8c7c531
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-09-24 22:54:16 -0500

    refactor(role-guard): remove non-needed code
    * chore(JWT): add type

--- src/auth/roles/role.guard.ts
+++ src/auth/roles/role.guard.ts
@@ -1,21 +1,26 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 
+import { User } from "@/user/user";
+
 @Injectable()
 export class RoleGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
-  matchRoles(roles: string[], userRole: string) {
-    return roles.some((role) => role.includes(userRole));
-  }
-
   canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.get<string[]>("roles", context.getHandler());
-    if (!roles) {
+    const allowedEndpointRoles = this.reflector.get<string[] | undefined>(
+      "roles",
+      context.getHandler(),
+    );
+
+    if (!allowedEndpointRoles) {
       return true;
     }
 
-    const { user } = context.switchToHttp().getRequest();
-    return this.matchRoles(roles, user.roles);
+    const { user } = context.switchToHttp().getRequest<{ user: User }>();
+
+    return allowedEndpointRoles.some(
+      (endpointRole) => endpointRole === user.role,
+    );
   }
 }
