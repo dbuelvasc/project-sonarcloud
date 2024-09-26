64efa50b06a8388c85a3bb1bd59caee7184e69be
jjmontenegro <jj.montenegro@uniandes.edu.co>
2024-09-25 22:20:20 -0500

    chore(recipe): secure endpoints

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -4,16 +4,22 @@ import {
   Delete,
   Get,
   HttpCode,
+  HttpStatus,
   Param,
   Post,
   Put,
+  UseGuards,
   UseInterceptors,
 } from "@nestjs/common";
 
+import { JwtAuthGuard } from "@/auth/guards";
+import { RoleGuard } from "@/auth/roles/role.guard";
+import { Roles } from "@/auth/roles/roles.decorator";
 import {
   BusinessErrorsInterceptor,
   UUIDValidationInterceptor,
 } from "@/shared/interceptors";
+import { UserRoles } from "@/shared/security/userRoles";
 import { RecipeDto } from "./recipe.dto";
 import { RecipeService } from "./recipe.service";
 
@@ -22,25 +28,30 @@ import { RecipeService } from "./recipe.service";
   BusinessErrorsInterceptor,
   new UUIDValidationInterceptor("recipeId"),
 )
+@UseGuards(JwtAuthGuard, RoleGuard)
 export class RecipeController {
   constructor(private readonly recipeService: RecipeService) {}
 
   @Get()
+  @Roles(UserRoles.ADMIN, UserRoles.FULL_READER)
   async findAll() {
     return await this.recipeService.findAll();
   }
 
   @Get(":recipeId")
+  @Roles(UserRoles.ADMIN, UserRoles.FULL_READER, UserRoles.LIMITED_READER)
   async findOne(@Param("recipeId") recipeId: string) {
     return await this.recipeService.findOne(recipeId);
   }
 
   @Post()
+  @Roles(UserRoles.ADMIN, UserRoles.WRITER)
   async create(@Body() recipeDto: RecipeDto) {
     return await this.recipeService.create(recipeDto);
   }
 
   @Put(":recipeId")
+  @Roles(UserRoles.ADMIN, UserRoles.WRITER)
   async update(
     @Param("recipeId") recipeId: string,
     @Body() recipeDto: RecipeDto,
@@ -49,7 +60,8 @@ export class RecipeController {
   }
 
   @Delete(":recipeId")
-  @HttpCode(204)
+  @Roles(UserRoles.ADMIN, UserRoles.DELETE)
+  @HttpCode(HttpStatus.NO_CONTENT)
   async delete(@Param("recipeId") recipeId: string) {
     return await this.recipeService.delete(recipeId);
   }
