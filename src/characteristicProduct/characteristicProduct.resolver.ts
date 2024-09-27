24c8dfce53e12460498ed30b76b168d6a304ac36
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-09-26 19:35:14 -0500

    chore: run prettier

--- src/characteristicProduct/characteristicProduct.resolver.ts
+++ src/characteristicProduct/characteristicProduct.resolver.ts
@@ -1,8 +1,9 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { CharacteristicProductService } from "./characteristicProduct.service";
-import { CharacteristicProductEntity } from "./characteristicProduct.entity";
-import { CharacteristicProductDto } from "./characteristicProduct.dto";
 import { plainToInstance } from "class-transformer";
+
+import { CharacteristicProductDto } from "./characteristicProduct.dto";
+import { CharacteristicProductEntity } from "./characteristicProduct.entity";
+import { CharacteristicProductService } from "./characteristicProduct.service";
 
 @Resolver()
 export class CharacteristicProductResolver {
@@ -13,30 +14,41 @@ export class CharacteristicProductResolver {
     return this.service.findAll();
   }
 
-
   @Query(() => CharacteristicProductEntity)
-  async characteristicProduct(@Args('id') id: string): Promise<CharacteristicProductEntity> {
-      return this.service.findOne(id);
+  async characteristicProduct(
+    @Args("id") id: string,
+  ): Promise<CharacteristicProductEntity> {
+    return this.service.findOne(id);
   }
 
   @Mutation(() => CharacteristicProductEntity)
-  createCharacteristicProduct(@Args('categoriaProducto') CharacteristicProductDto: CharacteristicProductDto): Promise<CharacteristicProductEntity> {
-      const categoriaProducto = plainToInstance(CharacteristicProductEntity, CharacteristicProductDto);
-      return this.service.create(categoriaProducto);
+  createCharacteristicProduct(
+    @Args("categoriaProducto")
+    CharacteristicProductDto: CharacteristicProductDto,
+  ): Promise<CharacteristicProductEntity> {
+    const categoriaProducto = plainToInstance(
+      CharacteristicProductEntity,
+      CharacteristicProductDto,
+    );
+    return this.service.create(categoriaProducto);
   }
 
   @Mutation(() => CharacteristicProductEntity)
-    updateCharacteristicProduct(@Args('id') id: string, @Args('categoriaProducto') CharacteristicProductDto: CharacteristicProductDto): Promise<CharacteristicProductEntity> {
-        const categoriaProducto = plainToInstance(CharacteristicProductEntity, CharacteristicProductDto);
-        return this.service.update(id, categoriaProducto);
-    }
- 
-    @Mutation(() => String)
-    deletecharacteristicProduct(@Args('id') id: string) {
-        this.service.delete(id);
-        return id;
-    }
+  updateCharacteristicProduct(
+    @Args("id") id: string,
+    @Args("categoriaProducto")
+    CharacteristicProductDto: CharacteristicProductDto,
+  ): Promise<CharacteristicProductEntity> {
+    const categoriaProducto = plainToInstance(
+      CharacteristicProductEntity,
+      CharacteristicProductDto,
+    );
+    return this.service.update(id, categoriaProducto);
+  }
 
-
-
+  @Mutation(() => String)
+  deletecharacteristicProduct(@Args("id") id: string) {
+    this.service.delete(id);
+    return id;
+  }
 }
