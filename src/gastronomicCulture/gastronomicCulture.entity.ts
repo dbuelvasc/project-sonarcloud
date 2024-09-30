2af57a517a2b9e67ea1fac880b2734ebcecf6a9d
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-09-29 21:02:05 -0500

    feat(graphql-api): add gastronomic culture resolver

--- src/gastronomicCulture/gastronomicCulture.entity.ts
+++ src/gastronomicCulture/gastronomicCulture.entity.ts
@@ -11,21 +11,27 @@ import { CharacteristicProductEntity } from "@/characteristicProduct/characteris
 import { CountryEntity } from "@/country/country.entity";
 import { RecipeEntity } from "@/recipe/recipe.entity";
 import { RestaurantEntity } from "@/restaurant/restaurant.entity";
+import { Field, ObjectType } from "@nestjs/graphql";
 
+@ObjectType()
 @Entity()
 export class GastronomicCultureEntity {
+  @Field()
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @Field()
   @Column()
   name: string;
 
+  @Field(() => [CharacteristicProductEntity])
   @OneToMany(
     () => CharacteristicProductEntity,
     (characteristicProduct) => characteristicProduct.gastronomicCulture,
   )
   characteristicProducts: CharacteristicProductEntity[];
 
+  @Field(() => [RestaurantEntity])
   @ManyToMany(
     () => RestaurantEntity,
     (restaurant) => restaurant.gastronomicCultures,
@@ -33,10 +39,12 @@ export class GastronomicCultureEntity {
   @JoinTable()
   restaurants: RestaurantEntity[];
 
+  @Field(() => [CountryEntity])
   @ManyToMany(() => CountryEntity, (country) => country.gastronomicCultures)
   @JoinTable()
   countries: CountryEntity[];
 
+  @Field(() => [RecipeEntity])
   @OneToMany(() => RecipeEntity, (recipe) => recipe.gastronomicCulture)
   recipes: RecipeEntity[];
 }
