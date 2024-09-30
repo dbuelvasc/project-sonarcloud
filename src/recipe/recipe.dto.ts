1ac97fc2351096b21546d38495f616ea1280fd5a
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-09-29 21:12:05 -0500

    feat(graphql-api): add recipe resolver

--- src/recipe/recipe.dto.ts
+++ src/recipe/recipe.dto.ts
@@ -1,22 +1,29 @@
+import { Field, InputType } from "@nestjs/graphql";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
+@InputType()
 export class RecipeDto {
+  @Field()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @Field()
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @Field()
   @IsString()
   @IsNotEmpty()
   photo: string;
 
+  @Field()
   @IsString()
   @IsNotEmpty()
   preparationProcess: string;
 
+  @Field()
   @IsString()
   @IsOptional()
   video?: string;
